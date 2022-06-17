0b58c3bc9cb3e68661d2f1433ac7c3fd45ea2086
nicolas <nicolas.bouffanais.1999@gmail.com>
2022-06-16 22:00:57 -0400

    arregle el error del documento no esta definido, con un if, si no esta definida la ventana o el tipo de window, tomara el codigo de abajo y lo ejecutara

--- components/navbar.js
+++ components/navbar.js
@@ -40,17 +40,20 @@ const Navbar = () => {
     </nav>
   );
 };
-const hamburger = document.querySelector('.hamburger')
-const navMenu = document.querySelector('.nav-menu')
+if (typeof window !== "undefined") {
+  const hamburger = document.querySelector('.hamburger')
+  const navMenu = document.querySelector('.nav-menu')
 
-hamburger.addEventListener('click', () => {
-  hamburger.classList.toggle('active')
-  navMenu.classList.toggle('active')
-})
+  hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('active')
+    navMenu.classList.toggle('active')
+  })
 
-document.querySelectorAll('.nav-link').forEach(n => n.addEventListener('click', () => {
-  hamburger.classList.remove('active')
-  navMenu.classList.remove('active')
-}))
+  document.querySelectorAll('.nav-link').forEach(n => n.addEventListener('click', () => {
+    hamburger.classList.remove('active')
+    navMenu.classList.remove('active')
+  }))
+}
+
 
 export default Navbar;
